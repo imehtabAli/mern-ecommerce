76eeb608a0ee92974bf5b8c273365dd087e2ec33
imehtabAli <mehtabg0709@gmail.com>
2026-07-02 06:28:13 +0530

    fixed text casing

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { registerUser } from '../services/AuthService';
+import { registerUser } from '../services/authService';
 import { useAuth } from '../context/AuthContext';
 
 const Register = () => {
